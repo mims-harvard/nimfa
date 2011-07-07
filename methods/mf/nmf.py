1195d309ed596784860ad878a6ff367269f7ac3d
Marinka <marinka@zitnik.si>
2011-07-07 22:49:39 +0200

    linalg sparse operation

--- methods/mf/nmf.py
+++ methods/mf/nmf.py
@@ -1,5 +1,6 @@
 import numpy as np
-from operator import div
+from operator import div, pow
+from math import log
 
 from utils.linalg import *
 
@@ -33,13 +34,13 @@ class Nmf(object):
                       update equations and type of objective function. 
                       When specifying model, user can pass 'update' keyword argument with one of
                       possible values: 
-                          #. 'euclidean' for classic euclidean distance update equations, 
+                          #. 'euclidean' for classic Euclidean distance update equations, 
                           #. 'divergence' for divergence update equations.
                       When specifying model, user can pass 'objective' keyword argument with one of
                       possible values:
-                          #. 'euclidean' for standard euclidean distance cost function,
-                          #. 'divergence' for divergence of target matrix from NMF estimate cost function (KL),
-                          #. 'connectivity' for connectivity matrix changed elements cost function. 
+                          #. 'fro' for standard Frobenius distance cost function,
+                          #. 'div' for divergence of target matrix from NMF estimate cost function (KL),
+                          #. 'conn' for connectivity matrix changed elements cost function. 
                         Default are 'euclidean' update equations and 'euclidean' cost function. 
         :type model: :class:`models.nmf_std.Nmf_std`
         """
@@ -53,5 +54,19 @@ class Nmf(object):
         
     def divergence_update(self):
         """Update basis and mixture matrix based on divergence multiplicative update rules."""
-        # self.H = np.multiply(self.H / , self.W.T * (self.V / (self.W * self.H)))
+        x1 = repmat(self.W.sum(0).T, 1, self.V.shape[1])
+        self.H = multiply(self.H, elop(dot(self.W.T, elop(self.V, dot(self.W, self.H), div)), x1, div))
+        x2 = repmat(self.H.sum(1).T, self.V.shape[0], 1)
+        self.W = multiply(self.W, elop(dot(elop(self.V, dot(self.W, self.H), div), self.H.T), x2, div))
+        
+    def fro_error(self):
+        """Compute squared Frobenius norm of a target matrix and its NMF estimate.""" 
+        return (elop(self.V - dot(self.W, self.H), 2, pow)).sum()
+    
+    def div_error(self):
+        """Compute divergence of traget matrix from its NMF estimate."""
+        return (multiply(self.V, elop(self.V, dot(self.W, self.H), log)) - self.V + dot(self.W, self.H)).sum()
+    
+    def conn_error(self):
+        pass    
         
