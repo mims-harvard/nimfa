818703740e165aa6d8982ab7aa64747d210f8043
Marinka <marinka@zitnik.si>
2011-08-03 14:05:22 +0200

    modify auxiliaries

--- models/nmf_ns.py
+++ models/nmf_ns.py
@@ -34,6 +34,11 @@ class Nmf_ns(nmf.Nmf):
     def __init__(self, params):
         """
         Construct factorization model that manages nonsmooth NMF models.
+        
+        :param params: MF runtime and algorithm parameters and options. For detailed explanation of the general model 
+                       parameters see :mod:`mf_methods`. For algorithm specific model options see documentation of chosen
+                       factorization method. 
+        :type params: `dict`
         """
         nmf.Nmf.__init__(self, params)
         if not self.seed and not self.W and not self.H: self.seed = "random"
@@ -44,7 +49,7 @@ class Nmf_ns(nmf.Nmf):
                 self.seed = nmf.seed.fixed.Fixed() 
                 self.seed._set_fixed(self.W, self.H)
         self._is_smdefined()
-        if nmf.sp.isspmatrix(self.V) and any(self.V.data < 0) or any(self.V < 0):
+        if nmf.sp.isspmatrix(self.V) and (self.V.data < 0).any() or (self.V < 0).any():
             raise nmf.utils.MFError("The input matrix contains negative elements.") 
         
     def basis(self):
