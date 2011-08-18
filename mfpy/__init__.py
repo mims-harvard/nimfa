2ce7785f674cdb636b211a3d603d91051246e340
Marinka <marinka@zitnik.si>
2011-08-18 19:25:31 +0200

    change mf directory structure

---
+++ mfpy/__init__.py
@@ -0,0 +1,9 @@
+
+"""
+    MF is a Python library which includes a number of published matrix factorization algorithms, 
+    initialization methods, quality and performance measures and facilitates the combination of these to produce new strategies. 
+    The library represents a unified and efficient interface to matrix factorization algorithms and methods.
+    
+"""
+
+from mf import *
