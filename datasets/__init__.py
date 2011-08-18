0e6340891c3b2d3ed1a5fae29e82c400816f050b
Marinka <marinka@zitnik.si>
2011-08-18 12:47:24 +0200

    package description

---
+++ datasets/__init__.py
@@ -0,0 +1,5 @@
+
+"""
+    This package contains nonsynthetic data sets that are used in examples of usage. 
+
+"""
--- examples/__init__.py
+++ examples/__init__.py
@@ -1,1 +1,13 @@
+
+"""
+    This package contains examples of usage of MF library. It demonstrates the following:
+        
+        #. Single run of the specified factorization algorithm.
+        #. Multiple runs of the specified factorization algorithm.
+        #. Tracking fitted results across multiple runs and tracking residuals error across single/mutliple runs.
+        #. Quality and performance measures of executed factorizations.
+        #. Running factorization algorithms with algorithm specific settings and initializations. 
+
+"""
+
 import synthetic
