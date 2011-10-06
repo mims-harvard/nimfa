8ecfd5cab422d5eeef12d3dc78e415ccd2f1aef7
Marinka Zitnik <marinka@zitnik.si>
2011-10-06 13:06:35 +0200

    update docs for callback_init

--- mf/mf_run.py
+++ mf/mf_run.py
@@ -72,11 +72,11 @@ def mf(target, seed = None, W = None, H = None,
                      function is called with only one argument :class:`models.mf_fit.Mf_fit` that contains the fitted model. Default is None.
     :type callback: `function`
     :param callback_init: Pass a callback function that is called after each initialization of the matrix factors. In case of multiple runs
-                          the function is called before each run (more precisely after initialization and before the factorization). In case
+                          the function is called before each run (more precisely after initialization and before the factorization of each run). In case
                           of single run, the passed callback function is called after the only initialization of the matrix factors. This is 
-                          useful if one wants to obtain the initialized matrix factors for further analysis. The callback function is called
-                          with only one argument :class:`models.mf_fit.Mf_fit` that (among others) contains also initialized matrix factors.
-                          Default is None. 
+                          useful if one wants to obtain the initialized matrix factors for further analysis or additional info about initialized
+                          factorization model. The callback function is called with only one argument :class:`models.mf_fit.Mf_fit` that (among others) 
+                          contains also initialized matrix factors. Default is None. 
     :type callback_init: `function`
     :param initialize_only: The specified MF model and its parameters will only be initialized. Model initialization includes:
                                 #. target matrix format checking and possibly conversion into one of accepting formats,
