cd1b81cb594a814e87afc657119b3e838c96943e
Marinka Zitnik <marinka@zitnik.si>
2012-09-03 18:56:41 -0400

    update example usage docs

--- docs/source/code/usage.py
+++ docs/source/code/usage.py
@@ -235,10 +235,10 @@ print 'Sparseness, W: %5.4f, H: %5.4f' % fctr_res.fit.sparseness()
 ####
 
 
-# Import nimfa library entry point for factorization
+# Import nimfa library entry point for factorization.
 import nimfa
 
-# Here we will work with numpy matrix
+# Here we will work with numpy matrix.
 import numpy as np
 V = np.random.random((23, 200))
 
@@ -252,7 +252,7 @@ fctr_res = nimfa.mf_run(fctr)
 # Print the loss function according to Kullback-Leibler divergence. 
 print "Distance Kullback-Leibler: %5.3e" % fctr_res.distance(metric = "kl")
 
-# Compute generic set of measures to evaluate the quality of the factorization
+# Compute generic set of measures to evaluate the quality of the factorization.
 sm = fctr_res.summary()
 # Print residual sum of squares.
 print "Rss: %8.3f" % sm['rss']
@@ -269,33 +269,33 @@ print "cophenetic: %8.3f" % sm['cophenetic']
 ####
 
 
-# Import nimfa library entry point for factorization
+# Import nimfa library entry point for factorization.
 import nimfa
 
-# Here we will work with numpy matrix
+# Here we will work with numpy matrix.
 import numpy as np
 
-# Generate random target matrix
+# Generate random target matrix.
 V = np.random.rand(30, 20)
 
-# Generate random matrix factors which we will pass as fixed factors to Nimfa
+# Generate random matrix factors which we will pass as fixed factors to Nimfa.
 init_W = np.random.rand(30, 4)
 init_H = np.random.rand(4, 20)
-# Obviously by passing these factors we want to use rank = 4
+# Obviously by passing these factors we want to use rank = 4.
 
 # Run NMF.
 # We don't specify any algorithm parameters. Defaults will be used.
-# We specify fixed initialization method and pass matrix factors
+# We specify fixed initialization method and pass matrix factors.
 fctr = nimfa.mf(V, method = "nmf", seed = "fixed", W = init_W, H = init_H, rank = 4)
 fctr_res = nimfa.mf_run(fctr)
 
 # Print the loss function (Euclidean distance between target matrix and its estimate). 
 print "Euclidean distance: %5.3e" % fctr_res.distance(metric = "euclidean")
 
-# It should print 'fixed'
+# It should print 'fixed'.
 print fctr_res.seeding
 
-# By default, max 30 iterations are performed
+# By default, max 30 iterations are performed.
 print fctr_res.n_iter
 
 
