911b8149a6c8a39f652748041e915f8c63863700
Yaroslav Halchenko <debian@onerussian.com>
2015-12-15 16:29:44 -0500

    RF: use magical monkeypatch from py.test to patch in check_synthetic_result

--- nimfa/tests/test_examples.py
+++ nimfa/tests/test_examples.py
@@ -10,9 +10,11 @@ synthetic_runs = [getattr(synthetic, f) for f in dir(synthetic) if f.startswith(
 
 @pytest.mark.medium
 @pytest.mark.parametrize('synth_run', synthetic_runs)
-def test_synthetic(synth_run):
+def test_synthetic(synth_run, monkeypatch):
+    ran_funcs = []
     def check_synthetic_result(fit, idx=None):
         """Basic checks that synthetic results are 'Ok'"""
+        ran_funcs.append(1)
         # typically tests pass with > .75 but because of still present inherent
         # randomizations results vary. So to be on a safe side for now set a very
         # liberal threshold
@@ -25,12 +27,10 @@ def test_synthetic(synth_run):
     V = prng.rand(20, 30)
     V1 = prng.rand(20, 25)
 
-    try:
-        old_print_info = synthetic.print_info
-        synthetic.print_info = check_synthetic_result
-        if synth_run is getattr(synthetic, 'run_snmnmf'):
-            synth_run(V, V1)
-        else:
-            synth_run(V)
-    finally:
-        synthetic.print_info = old_print_info
+    monkeypatch.setattr(synthetic, 'print_info', check_synthetic_result)
+    if synth_run is getattr(synthetic, 'run_snmnmf'):
+        synth_run(V, V1)
+    else:
+        synth_run(V)
+    # for paranoid monkeypatching people, to make sure that the patch worked
+    assert ran_funcs != [], "we must have ran our check"
